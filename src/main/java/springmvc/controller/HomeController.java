fba8a85d8178d971453d8bbe1650c6c17e57bfd8
Amethyst00007 <anusha.7.singh@gmail.com>
2026-08-13 22:09:40 +0530

    Initial commit - Spring MVC project

---
+++ src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,41 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String home(Model model) {
+		System.out.println("This is our home url");
+		model.addAttribute("name","Anusha Singh");
+		model.addAttribute("id",79);
+		return "index";
+		
+	}
+	
+     @RequestMapping("/about")
+	  public String about() {
+			System.out.println("This is about home url");
+			return "about";		
+	}
+     
+     @RequestMapping("/help")
+	  public ModelAndView help() {
+			System.out.println("This is about help url");
+			ModelAndView mav = new ModelAndView();
+			
+			mav.addObject("name","Anusha");
+			mav.addObject("id",9876);
+			
+			
+			
+			mav.setViewName("help");
+			
+			return mav;		
+	}
+
+}
